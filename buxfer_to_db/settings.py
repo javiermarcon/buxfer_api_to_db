6577c441822e16187b85e1f37c0a3bf8598209bb
Javier Marcon <javiermarcon@gmail.com>
2020-08-07 00:13:19 -0300

    moved secret key to custom settings

--- buxfer_to_db/settings.py
+++ buxfer_to_db/settings.py
@@ -22,14 +22,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'q65!@d0jz&+k(f@cw0bmwbz)a+n-js!^9x^z*&@h%2*f8$k+t*'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
 
 # Application definition
 
